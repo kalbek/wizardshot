d19a530328b5ae1836a5848c4930a0d8cd717448
KalebBekele <Nonst00pCbe0912>
2024-03-28 23:08:00 +0300

    Create gradient background for feature cards.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         muted: "#666666",
         darkGray: "#4F4F4F",
+        grayBase: "#363636",
         blackTransparent: "rgba(0, 0, 0, 0.56)",
         buttonPrimary: "#18181C",
         buttonSecondary: "#B900",
@@ -27,6 +28,7 @@ module.exports = {
       fontSize: {
         sm: "14px",
         base: "1rem",
+        upperbase: "18px",
         big: "20px",
         bigger: "32px",
         large: "40px",
@@ -38,6 +40,8 @@ module.exports = {
       backgroundImage: {
         "custom-gradient":
           "linear-gradient(to bottom, rgba(171, 150, 248, 1) 0%, rgba(217, 152, 226, 1) 50%, rgba(255, 153, 208, 1) 100%)",
+        "categories-gradient":
+          "linear-gradient(to right, rgba(68, 255, 154, 1) 0%, rgba(68, 176, 255, 1) 23%, rgba(100, 127, 255, 1) 35%,rgba(139, 68, 255, 1) 49%,  rgba(255, 102, 68, 1) 74%, rgba(235, 255, 112, 1) 100%)",
       },
       height: {
         smallVideoFrame: "247px",
