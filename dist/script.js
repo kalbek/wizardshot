5ee8947a688ca5eaeebed0aa3f38b597a9aa6eea
KalebBekele <Nonst00pCbe0912>
2024-03-28 15:56:48 +0300

    Complete header section.

--- dist/script.js
+++ dist/script.js
@@ -8,22 +8,24 @@ menuButton.addEventListener("click", () => {
   menuButton.classList.toggle("hidden");
   closeButton.classList.toggle("hidden");
   navigation.classList.toggle("hidden");
+  // loginSection.classList.toggle("hidden");
   // hide download wizarshot and login sections
-  loginSection.classList.remove("invisible");
+  loginSection.classList.remove("hidden");
   loginSection.classList.add("visible");
   // hide login text
-  //   loginText.classList.remove("invisible");
-  //   loginText.classList.add("visible");
+  // loginText.classList.remove("invisible");
+  // loginText.classList.add("visible");
 });
 
 closeButton.addEventListener("click", () => {
   menuButton.classList.toggle("hidden");
   closeButton.classList.toggle("hidden");
   navigation.classList.toggle("hidden");
+  // loginSection.classList.toggle("hidden");
   // show download wizarshot and login sections
   loginSection.classList.remove("visible");
-  loginSection.classList.add("invisible");
+  loginSection.classList.add("hidden");
   // hide login text
-  //   loginText.classList.add("invisible");
-  //   loginText.classList.remove("visible");
+  // loginText.classList.add("invisible");
+  // loginText.classList.remove("visible");
 });
